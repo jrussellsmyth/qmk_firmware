a339674b1df7a1f46e1a675a2dc0531899cab9a7
J. Russell Smyth <jrussell.smyth@gmail.com>
2025-01-06 19:23:21 +0000

    update devcontainer and keymap.c for mostlyprinted

--- keyboards/quadsmack/mostlyprinted/keymaps/default/keymap.c
+++ keyboards/quadsmack/mostlyprinted/keymaps/default/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,      KC_W,        KC_E,        KC_R,        KC_T,          KC_Y,    KC_U,        KC_I,        KC_O,        KC_P,
         KC_A,      ALT_T(KC_S), CTL_T(KC_D), SFT_T(KC_F), KC_G,          KC_H,    SFT_T(KC_J), CTL_T(KC_K), ALT_T(KC_L), KC_SCLN,
         KC_Z,      KC_X,        KC_C,        KC_V,        KC_B,          KC_N,    KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,
-        /*NO-KEY*/ KC_LSFT,     LT(_MOUSE,KC_ESC),      LT(_NAV,KC_SPC),      LT(_MOUSE,KC_TAB),        LT(_SYM,KC_ENT),  LT(_NUM,KC_BSPC),     LT(_FUN,KC_DEL),     KC_RSFT      /*NO-KEY*/
+        /*NO-KEY*/ KC_LSFT,   XXXXXXX,      LT(_NAV,KC_SPC),      LT(_MOUSE,KC_TAB),        LT(_SYM,KC_ENT),  LT(_NUM,KC_BSPC),     LT(_FUN,KC_DEL),     KC_RSFT      /*NO-KEY*/
     ),
     /* Navigation */
     [_NAV] = LAYOUT(
@@ -49,22 +49,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     /* Number */
     [_NUM] = LAYOUT(
-        _______,   KC_7,      KC_8,     KC_9,       KC_T,        _______,   _______,  _______,   _______,  _______,
-        _______,   KC_4,      KC_5,     KC_6,       KC_G,        KC_H,      _______,  _______,   _______,  _______,
-        _______,   KC_1,      KC_2,     KC_3,       KC_B,        _______,   _______,  _______,   _______,  _______,
-        /*NO-KEY*/ _______,   _______,  _______,    _______,     _______,   _______,  _______,   _______  /*NO-KEY*/
+        KC_LBRC,   KC_7,      KC_8,     KC_9,       KC_RBRC,        XXXXXXX,   XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,
+        KC_SCLN,   KC_4,      KC_5,     KC_6,       KC_EQUAL,       XXXXXXX,      KC_RSFT,  KC_RCTL,   KC_RALT,  XXXXXXX,
+        KC_GRAVE,  KC_1,      KC_2,     KC_3,       KC_BSLS,        XXXXXXX,   XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,
+        /*NO-KEY*/ XXXXXXX,   XXXXXXX,  XXXXXXX,    XXXXXXX,        XXXXXXX,   _______,  XXXXXXX,   XXXXXXX  /*NO-KEY*/
     ),
     /* Symbol */
     [_SYM] = LAYOUT(
-        KC_LCBR,   KC_AMPR,   KC_ASTR,  KC_LPRN,    KC_T,            KC_Y,    KC_U,        KC_I,        KC_O,        KC_P,
-        KC_COLN,   KC_DLR,    KC_PERC,  KC_CIRC,    KC_G,            KC_H,    SFT_T(KC_J), CTL_T(KC_K), ALT_T(KC_L), KC_SCLN,
-        KC_TILD,   KC_EXLM,   KC_AT,    KC_HASH,    KC_B,            KC_N,    KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,
-        /*NO-KEY*/ _______,   _______,  _______,    _______,         _______,   _______,  _______,      _______   /*NO-KEY*/
+        KC_LCBR,   KC_AMPR,   KC_ASTR,  KC_LPRN,    KC_RCBR,         XXXXXXX,   XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,
+        KC_COLN,   KC_DLR,    KC_PERC,  KC_CIRC,    KC_PLUS,         XXXXXXX,      KC_RSFT,  KC_RCTL,   KC_RALT,  XXXXXXX,
+        KC_TILD,   KC_EXLM,   KC_AT,    KC_HASH,    KC_PIPE,         XXXXXXX,   XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,
+        /*NO-KEY*/  XXXXXXX,   XXXXXXX,  XXXXXXX,    XXXXXXX,        _______,  XXXXXXX,  XXXXXXX,   XXXXXXX  /*NO-KEY*/
     ),
     [_FUN] = LAYOUT(
-        KC_F12,      KC_F7,     KC_F8,    KC_F9,        _______,        _______,   _______,  _______,      _______,  _______,
-        KC_F11,      KC_F4,     KC_F5,    KC_F6,        _______,        _______,   _______,  _______,      _______,  _______,
-        KC_F10,      KC_F1,     KC_F2,    KC_F3,        _______,        _______,   _______,  _______,      _______,  _______,
-        /*NO-KEY*/   _______,   _______,  _______,      _______,        _______,   _______,  _______,      _______  /*NO-KEY*/
+        KC_F12,      KC_F7,     KC_F8,    KC_F9,        XXXXXXX,     XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,
+        KC_F11,      KC_F4,     KC_F5,    KC_F6,        XXXXXXX,     XXXXXXX,  KC_RSFT,  KC_RCTL,   KC_RALT,  XXXXXXX,
+        KC_F10,      KC_F1,     KC_F2,    KC_F3,        XXXXXXX,     XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,
+        /*NO-KEY*/   XXXXXXX,   XXXXXXX,  XXXXXXX,      XXXXXXX,     XXXXXXX,  XXXXXXX,  _______,   XXXXXXX  /*NO-KEY*/
     )
 };
