b97bc7fafaff09a2f2dc1680d4074061bea2a88b
J. Russell Smyth <jrussell.smyth@gmail.com>
2025-01-18 08:45:09 +0000

    keymap update

--- keyboards/quadsmack/mostlyprinted/keymaps/default/keymap.c
+++ keyboards/quadsmack/mostlyprinted/keymaps/default/keymap.c
@@ -12,8 +12,7 @@ enum layers {
     _FUN
 };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-     /*
-    * ┌───┬─────┬─────┬─────┬─────┐       ┌─────┬─────┬─────┬─────┬─────┐
+   /* ┌───┬─────┬─────┬─────┬─────┐       ┌─────┬─────┬─────┬─────┬─────┐
     * │ Q │  W  │  E  │  R  │  T  │       │  Y  │  U  │  I  │  O  │  P  │
     * ├───┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┤
     * │ A │S/ALT│D/CMD│F/SFT│  G  │       │  H  │J/SFT│K/CMD│L/ALT│  ;  │
@@ -22,16 +21,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * └───┴─────┴─────┴─────┴─────┘       └─────┴─────┴─────┴─────┴─────┘
     *               ┌───┬───┐                   ┌───┬───┐
     *               │   │ESC├───┐           ┌───┤DEL│   │
-    *               └───┴───┤SPC├───┐   ┌───┤BSP├───┴───┘
+    *               └───┴───┤BSP├───┐   ┌───┤SPC├───┴───┘
     *                       └───┤TAB│   │ENT├───┘
     *                           └───┘   └───┘
     */
     /* Base */
     [_QWERTY] = LAYOUT(
-        KC_Q,      KC_W,        KC_E,        KC_R,        KC_T,          KC_Y,    KC_U,        KC_I,        KC_O,        KC_P,
-        KC_A,      ALT_T(KC_S), CTL_T(KC_D), SFT_T(KC_F), KC_G,          KC_H,    SFT_T(KC_J), CTL_T(KC_K), ALT_T(KC_L), KC_SCLN,
-        KC_Z,      KC_X,        KC_C,        KC_V,        KC_B,          KC_N,    KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,
-        /*NO-KEY*/ KC_LSFT,   XXXXXXX,      LT(_NAV,KC_SPC),      LT(_MOUSE,KC_TAB),        LT(_SYM,KC_ENT),  LT(_NUM,KC_BSPC),     LT(_FUN,KC_DEL),     KC_RSFT      /*NO-KEY*/
+        KC_Q,      KC_W,        KC_E,        KC_R,        KC_T,  /**/  KC_Y,    KC_U,        KC_I,        KC_O,        KC_P,
+        KC_A,      ALT_T(KC_S), CTL_T(KC_D), SFT_T(KC_F), KC_G,  /**/  KC_H,    SFT_T(KC_J), CTL_T(KC_K), ALT_T(KC_L), KC_P,
+        KC_Z,      KC_X,        KC_C,        KC_V,        KC_B,  /**/  KC_N,    KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,
+        /*NO-KEY*/ KC_LSFT, KC_ESC, LT(_NAV,KC_BSPC), LT(_MOUSE,KC_TAB), /**/ LT(_SYM,KC_ENT),  LT(_NUM,KC_SPACE),     LT(_FUN,KC_DEL),     KC_RSFT      /*NO-KEY*/
     ),
     /* Navigation */
     [_NAV] = LAYOUT(
@@ -55,6 +54,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /*NO-KEY*/ XXXXXXX,   XXXXXXX,  XXXXXXX,    XXXXXXX,        XXXXXXX,   _______,  XXXXXXX,   XXXXXXX  /*NO-KEY*/
     ),
     /* Symbol */
+   /* ┌───┬─────┬─────┬─────┬─────┐       ┌─────┬─────┬─────┬─────┬─────┐
+    * │ Q │  W  │  E  │  R  │  T  │       │  Y  │  U  │  I  │  O  │  P  │
+    * ├───┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┤
+    * │ A │S/ALT│D/CMD│F/SFT│  G  │       │  H  │J/SFT│K/CMD│L/ALT│  ;  │
+    * ├───┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┤
+    * │ Z │  X  │  C  │  V  │  B  │       │  N  │  M  │  ,  │  .  │  /  │
+    * └───┴─────┴─────┴─────┴─────┘       └─────┴─────┴─────┴─────┴─────┘
+    *               ┌───┬───┐                   ┌───┬───┐
+    *               │   │ESC├───┐           ┌───┤DEL│   │
+    *               └───┴───┤BSP├───┐   ┌───┤SPC├───┴───┘
+    *                       └───┤TAB│   │ENT├───┘
+    *                           └───┘   └───┘
+    */
     [_SYM] = LAYOUT(
         KC_LCBR,   KC_AMPR,   KC_ASTR,  KC_LPRN,    KC_RCBR,         XXXXXXX,   XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,
         KC_COLN,   KC_DLR,    KC_PERC,  KC_CIRC,    KC_PLUS,         XXXXXXX,      KC_RSFT,  KC_RCTL,   KC_RALT,  XXXXXXX,
